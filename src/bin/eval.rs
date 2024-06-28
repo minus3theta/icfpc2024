54f9c08b3a175ba813b809110289411166d1d13f
minus3theta <yone_j_synthesis@yahoo.co.jp>
2024-06-29 03:43:47 +0900

    Add eval.rs

---
+++ src/bin/eval.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+use icfpc2024::{ast::Expr, token};
+
+fn main() -> anyhow::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    let tokens = token::decode_token_stream(input.trim())?;
+    let ast = Expr::from_tokens(&tokens)?;
+    println!("{:?}", ast.eval(&vec![])?);
+
+    Ok(())
+}
