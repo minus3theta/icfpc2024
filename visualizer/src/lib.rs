6fdc42271744b6f5a1a3028c5067404e8419a66c
Fumiya Suto <fumiya.suto@gmail.com>
2024-06-29 07:42:35 +0900

    added visualizer for spaceship (WIP)

--- visualizer/src/lib.rs
+++ visualizer/src/lib.rs
@@ -1,5 +1,6 @@
 mod lambdaman;
 mod shape;
+mod spaceship;
 
 use shape::*;
 use wasm_bindgen::prelude::*;
@@ -10,13 +11,19 @@ pub fn lambdaman_gen(seed: i32) -> String {
     format!("{}", input)
 }
 
+pub fn spaceship_gen(seed: i32) -> String {
+    let input = spaceship::gen_input(seed as u64);
+    // convert input to string:
+    format!("{}", input)
+}
+
 #[wasm_bindgen]
 pub fn gen(seed: i32, game_type: String) -> String {
     if game_type.starts_with("lambdaman") {
         return lambdaman_gen(seed);
     }
     if game_type == "spaceship" {
-        return "".to_string();
+        return spaceship_gen(seed);
     }
     "".to_string()
 }
@@ -92,17 +99,115 @@ pub fn lambdaman_vis(_input: String, _output: String, turn: usize) -> Ret {
     }
 }
 
+fn spaceship_visualize(
+    input: &spaceship::Input,
+    visited: &std::collections::HashSet<(i32, i32)>,
+    pos_history: &std::collections::VecDeque<(i32, i32)>,
+) -> String {
+    let mut min_x = input.min_x;
+    let mut min_y = input.min_y;
+    let mut max_x = input.max_x;
+    let mut max_y = input.max_y;
+    let width = input.max_x - input.min_x + 3;
+    let height = input.max_y - input.min_y + 3;
+    if width > height {
+        min_y -= (width - height) / 2;
+        max_y += (width - height + 1) / 2;
+    } else {
+        min_x -= (height - width) / 2;
+        max_x += (height - width + 1) / 2;
+    }
+    let remains = input
+        .target
+        .difference(visited)
+        .cloned()
+        .collect::<std::collections::HashSet<(i32, i32)>>();
+    let n = (max_x - min_x + 1) as usize;
+    if n < 160 {
+        let size = 800 / n;
+        let whole = size * n;
+        let mut svg = document(whole, whole);
+        // draw each grid. represent number as color.
+        for i in 0..n {
+            for j in 0..n {
+                svg = svg.add(rect_with_stroke(
+                    (j * size) as usize,
+                    (i * size) as usize,
+                    size as usize,
+                    size as usize,
+                    "white",
+                ));
+            }
+        }
+        for &p in &remains {
+            let x = (p.0 - min_x) as usize;
+            let y = (n as i32 - 1 - (p.1 - min_y as i32)) as usize;
+            svg = svg.add(rect_with_stroke(
+                (x * size) as usize,
+                (y * size) as usize,
+                size as usize,
+                size as usize,
+                "gray",
+            ));
+        }
+        let base = pos_history.capacity() - pos_history.len();
+        for (idx, &p) in pos_history.iter().enumerate() {
+            let x = (p.0 - min_x) as usize;
+            let y = (n as i32 - 1 - (p.1 - min_y)) as usize;
+            svg = svg.add(circle(
+                x * size + size / 2,
+                y * size + size / 2,
+                size / 4,
+                &color((idx + base) as f64 / pos_history.capacity() as f64),
+            ));
+        }
+        svg.to_string()
+    } else {
+        let mut svg = document(800, 800);
+        for &p in &remains {
+            let ratio_x = (p.0 - min_x) as f64 / (max_x - min_x) as f64;
+            let ratio_y = 1.0 - (p.1 - min_y) as f64 / (max_y - min_y) as f64;
+            let x = (ratio_x * 780.0 + 10.0) as usize;
+            let y = (ratio_y * 780.0 + 10.0) as usize;
+            svg = svg.add(rect_with_stroke(x - 4, y - 4, 8, 8, "gray"));
+        }
+        let base = pos_history.capacity() - pos_history.len();
+        for (idx, &p) in pos_history.iter().enumerate() {
+            let ratio_x = (p.0 - min_x) as f64 / (max_x - min_x) as f64;
+            let ratio_y = 1.0 - (p.1 - min_y) as f64 / (max_y - min_y) as f64;
+            let x = (ratio_x * 780.0 + 10.0) as usize;
+            let y = (ratio_y * 780.0 + 10.0) as usize;
+            svg = svg.add(circle(
+                x,
+                y,
+                8,
+                &color((idx + base) as f64 / pos_history.capacity() as f64),
+            ));
+        }
+
+        svg.to_string()
+    }
+}
+
+pub fn spaceship_vis(_input: String, _output: String, turn: usize) -> Ret {
+    let input = spaceship::parse_input(&_input);
+    let output = spaceship::parse_output(&input, &_output).unwrap();
+    let (score, err, (visited, pos_history)) =
+        spaceship::compute_score_details(&input, &output.moves, turn);
+    Ret {
+        score: score,
+        err: err,
+        svg: spaceship_visualize(&input, &visited, &pos_history).to_string(),
+    }
+}
+
 #[wasm_bindgen]
 pub fn vis(_input: String, _output: String, turn: usize, game_type: String) -> Ret {
     if game_type.starts_with("lambdaman") {
         return lambdaman_vis(_input, _output, turn);
     }
     if game_type == "spaceship" {
-        return Ret {
-            score: 0,
-            err: "Not implemented".to_string(),
-            svg: "".to_string(),
-        };
+        return spaceship_vis(_input, _output, turn);
     }
     Ret {
         score: 0,
@@ -119,13 +224,21 @@ fn lambdaman_get_max_turn(_input: String, _output: String) -> usize {
         .len()
 }
 
+fn spaceship_get_max_turn(_input: String, _output: String) -> usize {
+    let input = lambdaman::parse_input(&_input);
+    lambdaman::parse_output(&input, &_output)
+        .unwrap()
+        .moves
+        .len()
+}
+
 #[wasm_bindgen]
 pub fn get_max_turn(_input: String, _output: String, game_type: String) -> usize {
     if game_type.starts_with("lambdaman") {
         return lambdaman_get_max_turn(_input, _output);
     }
     if game_type == "spaceship" {
-        return 0;
+        return spaceship_get_max_turn(_input, _output);
     }
     0
 }
---
+++ visualizer/src/spaceship.rs
@@ -0,0 +1,193 @@
+// copy from tools implementation at the following page
+// https://atcoder.jp/contests/masters-qual/tasks/masters_qual_a
+
+#![allow(non_snake_case, dead_code)]
+
+use std::ops::RangeBounds;
+
+pub trait SetMinMax {
+    fn setmin(&mut self, v: Self) -> bool;
+    fn setmax(&mut self, v: Self) -> bool;
+}
+impl<T> SetMinMax for T
+where
+    T: PartialOrd,
+{
+    fn setmin(&mut self, v: T) -> bool {
+        *self > v && {
+            *self = v;
+            true
+        }
+    }
+    fn setmax(&mut self, v: T) -> bool {
+        *self < v && {
+            *self = v;
+            true
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Input {
+    pub target: std::collections::HashSet<(i32, i32)>,
+    pub min_x: i32,
+    pub max_x: i32,
+    pub min_y: i32,
+    pub max_y: i32,
+}
+
+pub fn parse_input(f: &str) -> Input {
+    let mut target = std::collections::HashSet::new();
+    let mut min_x = 0;
+    let mut max_x = 0;
+    let mut min_y = 0;
+    let mut max_y = 0;
+
+    for line in f.lines() {
+        let mut parts = line.split_whitespace();
+        if let (Some(x), Some(y)) = (parts.next(), parts.next()) {
+            if let (Ok(x), Ok(y)) = (x.parse::<i32>(), y.parse::<i32>()) {
+                target.insert((x, y));
+                min_x.setmin(x);
+                max_x.setmax(x);
+                min_y.setmin(y);
+                max_y.setmax(y);
+            }
+        }
+    }
+    Input {
+        target,
+        min_x,
+        max_x,
+        min_y,
+        max_y,
+    }
+}
+
+pub fn read<T: Copy + PartialOrd + std::fmt::Display + std::str::FromStr, R: RangeBounds<T>>(
+    token: Option<&str>,
+    range: R,
+) -> Result<T, String> {
+    if let Some(v) = token {
+        if let Ok(v) = v.parse::<T>() {
+            if !range.contains(&v) {
+                Err(format!("Out of range: {}", v))
+            } else {
+                Ok(v)
+            }
+        } else {
+            Err(format!("Parse error: {}", v))
+        }
+    } else {
+        Err("Unexpected EOF".to_owned())
+    }
+}
+
+pub struct Output {
+    pub moves: Vec<u8>,
+}
+
+pub fn parse_output(_: &Input, f: &str) -> Result<Output, String> {
+    let mut moves = Vec::new();
+
+    for c in f.chars() {
+        if let Some(digit) = c.to_digit(10) {
+            if digit >= 1 && digit <= 9 {
+                moves.push((digit - 1) as u8);
+            } else {
+                return Err(format!("Character '{}' is out of the range 1-9", c));
+            }
+        } else {
+            return Err(format!("Character '{}' is not a valid digit", c));
+        }
+    }
+
+    Ok(Output { moves })
+}
+
+const FIXED: [&'static str; 26] = [
+    "",
+    include_str!("../../data/spaceship/spaceship1.in"),
+    include_str!("../../data/spaceship/spaceship2.in"),
+    include_str!("../../data/spaceship/spaceship3.in"),
+    include_str!("../../data/spaceship/spaceship4.in"),
+    include_str!("../../data/spaceship/spaceship5.in"),
+    include_str!("../../data/spaceship/spaceship6.in"),
+    include_str!("../../data/spaceship/spaceship7.in"),
+    include_str!("../../data/spaceship/spaceship8.in"),
+    include_str!("../../data/spaceship/spaceship9.in"),
+    include_str!("../../data/spaceship/spaceship10.in"),
+    include_str!("../../data/spaceship/spaceship11.in"),
+    include_str!("../../data/spaceship/spaceship12.in"),
+    include_str!("../../data/spaceship/spaceship13.in"),
+    include_str!("../../data/spaceship/spaceship14.in"),
+    include_str!("../../data/spaceship/spaceship15.in"),
+    include_str!("../../data/spaceship/spaceship16.in"),
+    include_str!("../../data/spaceship/spaceship17.in"),
+    include_str!("../../data/spaceship/spaceship18.in"),
+    include_str!("../../data/spaceship/spaceship19.in"),
+    include_str!("../../data/spaceship/spaceship20.in"),
+    include_str!("../../data/spaceship/spaceship21.in"),
+    "",
+    include_str!("../../data/spaceship/spaceship23.in"),
+    include_str!("../../data/spaceship/spaceship24.in"),
+    include_str!("../../data/spaceship/spaceship25.in"),
+];
+
+// change function name from `gen` to `gen_input`
+pub fn gen_input(seed: u64) -> String {
+    if seed as usize >= FIXED.len() {
+        return "".to_string();
+    }
+    FIXED[seed as usize].to_owned()
+}
+
+pub fn compute_score(input: &Input, out: &Output) -> (i64, String) {
+    let (mut score, err, _) = compute_score_details(input, &out.moves, out.moves.len());
+    if err.len() > 0 {
+        score = 0;
+    }
+    (score, err)
+}
+
+// added argument `max_turn`
+pub fn compute_score_details(
+    input: &Input,
+    moves: &Vec<u8>,
+    max_turn: usize,
+) -> (
+    i64,
+    String,
+    (
+        std::collections::HashSet<(i32, i32)>,
+        std::collections::VecDeque<(i32, i32)>,
+    ),
+) {
+    let mut pos = (0, 0);
+    let mut pos_history = std::collections::VecDeque::with_capacity(10);
+    let mut visited = std::collections::HashSet::new();
+    pos_history.push_back(pos);
+    for &dir in moves.iter().take(max_turn) {
+        pos.0 += dir as i32 % 3 - 1;
+        pos.1 += dir as i32 / 3 - 1;
+        if input.target.contains(&pos) {
+            visited.insert(pos);
+        }
+        if pos_history.len() == 10 {
+            pos_history.pop_front();
+        }
+        pos_history.push_back(pos);
+    }
+    if moves.len() == max_turn && input.target.len() != visited.len() {
+        return (
+            0,
+            format!(
+                "Unvisited squares remain: {}",
+                input.target.len() - visited.len()
+            ),
+            (visited, pos_history),
+        );
+    }
+    (max_turn as i64, String::new(), (visited, pos_history))
+}
+// end -- copy from ./tools/src/lib.rs
